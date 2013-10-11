9c3314bb78a679b012a778a03fc52237097dd216
Brad Milne <brad.milne@getelevate.com>
2013-10-11 13:28:31 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fearless::Application.config.secret_key_base = '50b49f3e0f643ceddb794f03ffd18330a24f08ea74fe60a40f1d6d6f781230e16b3d0e08d820ca4cb69d0e7bf2f8c5d2b83e567b71f11288187a6b1932669c78'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fearless::Application.config.session_store :cookie_store, key: '_fearless_session'
